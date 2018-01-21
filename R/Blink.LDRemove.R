9fe2bfa1ab54e98c29a0a8af4d8279e2ad100413
GST <zhouyao1989@163.com>
2018-01-21 17:21:53 +0800

    LDremove

--- R/Blink.LDRemove.R
+++ R/Blink.LDRemove.R
@@ -44,7 +44,8 @@
 #Authors: Yao Zhou
 #Last update: 08/15/2016
   #GDneo = as.matrix(GDneo)
-  GDneo = as.matrix(as.data.frame(GDneo))
+  write.table(GDneo,"GDneo.txt",col.names = F,row.names = F,quote=F,sep="\t")
+  GDneo = as.matrix(GDneo)
   SNP.index = apply(GDneo,1,sd)!=0
   GDneo = GDneo[SNP.index,]
   Porder = Porder[SNP.index]
